dfcdb07b799ac9df852c3c33a354b62b2c6101fd
chenggang0815 <954036868@qq.com>
2019-01-08 23:13:03 +0800

    add recursion

---
+++ basic-algo/7.java
@@ -0,0 +1,32 @@
+public class Recursion {
+
+    public static void recursion1(int n){
+        System.out.printf("1th - level: %d\n ",n);
+        if (n<3){
+            recursion1(n+1);
+        }
+        System.out.printf("2th - level: %d\n ",n);
+
+    }
+
+    public static int recursion2(int n){
+        if (n>3){
+            return n;
+        }
+        System.out.printf("1th - level: %d\n",n);
+
+        int result = recursion2(n+1);
+        System.out.printf("2th - level: %d\n",n);
+        System.out.printf("2th - result: %d\n",result);
+        return n;
+    }
+
+    public static void main(String[] args) {
+
+        //recursion1(1);
+        int result=recursion2(1);
+        System.out.printf("main - result: %d\n",result);
+
+    }
+}
+
