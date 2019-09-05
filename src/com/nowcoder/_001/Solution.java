36e5d05cb62cc6f983315708766e7b924ffa1c7b
chenggang0815 <954036868@qq.com>
2019-09-05 20:14:21 +0800

    update

--- src/com/nowcoder/_001/Solution.java
+++ src/com/nowcoder/_001/Solution.java
@@ -1,9 +1,8 @@
-package basic-algo._001;
-
+package com.nowcoder._001;
 //大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。 n<=39
 public class Solution {
     private static int Fibonacci(int n) {
-        else if(n ==0) return 0;
+        if(n ==0) return 0;
         else if(n ==1) return 1;
         else {
             return Fibonacci(n-1)+Fibonacci(n-2);
---
+++ src/com/nowcoder/_015/Solution.java
@@ -0,0 +1,74 @@
+// 单链表的复制
+public  class aa {
+	public static node cloneList(node head) {
+		node temp_head = head;
+		node clone_head = new node();
+		node clone_node = new node();
+
+		if (temp_head != null) {
+			clone_head.data = temp_head.data;
+			clone_head.next = null;
+			clone_node = clone_head;
+			temp_head = temp_head.next;
+		}
+		while (temp_head != null) {
+			node temp_node = new node(1);
+			temp_node.data = temp_head.data;
+			temp_node.next = null;
+			clone_node.next = temp_node;
+			clone_node = clone_node.next;
+			temp_head = temp_head.next;
+		}
+
+		return clone_head;
+	}
+
+
+	public static void copyList(Node head) {
+		Node node = head;
+		while (node != null) {
+			Node copyNode = new Node();
+			copyNode.value = node.value;
+			copyNode.next = node.next;
+			copyNode.sbiling = null;
+			node.next = copyNode;
+			node = copyNode.next;
+		}
+	}
+
+	public static void setSbiling(Node head) {
+		Node node = head;
+		while (node != null) {
+			Node copyNode = node.next;
+			if (node.sbiling != null) {
+				copyNode.sbiling = node.sbiling.next;
+			}
+			node = copyNode.next;
+		}
+	}
+
+	public static Node disConnectList(Node head) {
+		Node node = head;
+		Node copyHead = null;
+		Node copyNode = null;
+		if (node != null) {
+			copyHead = node.next;
+			copyNode = node.next;
+			node.next = copyNode.next;
+			node = node.next;
+		}
+		while (node != null) {
+			copyNode.next = node.next;
+			copyNode = copyNode.next;
+			node.next = copyNode.next;
+			node = node.next;
+		}
+		return copyHead;
+	}
+
+	public static Node copy(Node head) {
+		copyList(head);
+		setSbiling(head);
+		return disConnectList(head);
+	}
+}
