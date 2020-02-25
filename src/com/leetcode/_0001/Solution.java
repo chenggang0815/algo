d73c961292bb5b4ad9606e4c63a3fc64e7e685b4
chenggang <954036868@qq.com>
2020-02-25 10:06:01 +0800

    add two sum

--- src/com/leetcode/_0001/Solution.java
+++ src/com/leetcode/_0001/Solution.java
@@ -1,15 +1,35 @@
 package com.leetcode._0001;
+import java.util.Arrays;
 
+/*Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        Example:
+
+        Given nums = [2, 7, 11, 15], target = 9,
+
+        Because nums[0] + nums[1] = 2 + 7 = 9,
+        return [0, 1].
+*/
 public class Solution {
-    private static int Fibonacci(int n) {
-        if(n ==0) return 0;
-        else if(n ==1) return 1;
-        else {
-            return Fibonacci(n-1)+Fibonacci(n-2);
-        }
+    //  Brute Force
+    public static int[] twoSum(int[] nums, int target) {
+            for(int i = 0; i< nums.length-1; i++){
+                for(int j = i+1; j < nums.length; j++){
+                    if(nums[i] + nums[j] == target){
+                        int[] arr = new int[]{i,j};
+                        return arr;
+                    }
+                }
+            }
+            int[] arr = new int[]{};
+            return arr;
     }
 
+
     public static void main(String[] args) {
-        System.out.println(Fibonacci(3));
+        int[] array = new int[]{1,2,3,4};
+        System.out.println(Arrays.toString(twoSum(array,7)));
     }
 }
