2d94d56b3d5a8a517251433ec5f24b7999114b3b
chenggang <chenggang@qutoutiao.net>
2020-02-29 17:25:58 +0800

    Two Sum

--- src/com/leetcode/_0001/Solution.java
+++ src/com/leetcode/_0001/Solution.java
@@ -1,6 +1,5 @@
 package com.leetcode._0001;
 import java.util.Arrays;
-
 /*Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
         You may assume that each input would have exactly one solution, and you may not use the same element twice.
