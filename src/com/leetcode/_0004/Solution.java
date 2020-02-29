eaa198ab916ac0a0a5a7594435c3d82c796f11ed
chenggang <chenggang@qutoutiao.net>
2020-02-29 17:20:03 +0800

    Maximum Subarray

--- src/com/leetcode/_0004/Solution.java
+++ src/com/leetcode/_0004/Solution.java
@@ -13,7 +13,23 @@ Follow up:
 If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
  */
 public class Solution {
+    static int  maxSubArray(int[] nums){
+        int n=nums.length;
+        int maxSums=nums[0];
+        for (int i=0;i<n;i++){
+            int temp=0;
+            for (int j=i;j<n;j++){
+                temp=temp+nums[j];
+                if (temp>maxSums){
+                    maxSums=temp;
+                }
+            }
+        }
+        return maxSums;
+    }
     public static void main(String[] args) {
-        
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+
     }
 }
