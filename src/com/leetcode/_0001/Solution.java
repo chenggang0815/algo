6d70bedbde28f89745898751e251385fbe6088ab
chenggang <954036868@qq.com>
2020-03-01 20:27:20 +0800

    twoSum

--- src/com/leetcode/_0001/Solution.java
+++ src/com/leetcode/_0001/Solution.java
@@ -1,5 +1,7 @@
 package com.leetcode._0001;
 import java.util.Arrays;
+import java.util.HashMap;
+
 /*Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
         You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -13,6 +15,7 @@ import java.util.Arrays;
 */
 public class Solution {
     //  Brute Force
+    //Time complexity : O(n)
     public static int[] twoSum(int[] nums, int target) {
             for(int i = 0; i< nums.length-1; i++){
                 for(int j = i+1; j < nums.length; j++){
@@ -26,8 +29,39 @@ public class Solution {
             return arr;
     }
 
+    //Time complexity : O(n)
+    //Space complexity :O(n). The extra space required depends on the number of items stored in the hash table, which stores exactly nnn elements.
+    public static int[] twoSum2(int[] nums, int target){
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for (int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+
+        for (int i=0;i<nums.length;i++){
+            int value = target - nums[i];
+            if (map.containsKey(value) && map.get(value)!=i){
+                return new int[]{map.get(value),i};
+            }
+        }
+        return new int[]{};
+    }
+
+    public static int[] twoSum3(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            int value = target - nums[i];
+            if(map.containsKey(value) && map.get(value)!=i){
+                return new int[]{map.get(value),i};
+            }
+            map.put(nums[i],i);
+        }
+
+        return new int[]{};
+    }
+
+
     public static void main(String[] args) {
         int[] array = new int[]{1,2,3,4};
-        System.out.println(Arrays.toString(twoSum(array,7)));
+        System.out.println(Arrays.toString(twoSum2(array,7)));
     }
 }
