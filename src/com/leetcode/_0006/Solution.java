b852a5706e6496910dc08d04caa4fe972710203c
chenggang <chenggang@qutoutiao.net>
2020-03-03 20:46:12 +0800

    -

--- src/com/leetcode/_0006/Solution.java
+++ src/com/leetcode/_0006/Solution.java
@@ -1,7 +1,33 @@
 package com.leetcode._0006;
+/*
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
 
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ */
 public class Solution {
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val){
+            this.val = val;
+        }
+
+
+    }
     public static void main(String[] args) {
-        System.out.println("============");
+        //int[] nums = new int[]{0,9,9,9,0,0,0,9,0};
+
     }
 }
