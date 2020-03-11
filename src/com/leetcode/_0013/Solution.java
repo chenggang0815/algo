10b2682ad543ee5c08dc30b962ef1823daed4edf
chenggang <chenggang@qutoutiao.net>
2020-03-11 21:25:43 +0800

    -

---
+++ src/com/leetcode/_0013/Solution.java
@@ -0,0 +1,37 @@
+package com.leetcode._0013;
+
+import java.util.HashMap;
+
+/*
+Given an array of size n, find the majority element.
+The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+You may assume that the array is non-empty and the majority element always exist in the array.
+Example 1:
+Input: [3,2,3]
+Output: 3
+Example 2:
+Input: [2,2,1,1,1,2,2]
+Output: 2
+ */
+public class Solution {
+    static int majorityElement(int[] nums){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if (!map.containsKey(nums[i])){
+                map.put(nums[i],1);
+            }else {
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            if (map.get(nums[i])>nums.length/2){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+
+    }
+}
---
+++ src/com/leetcode/_0015/Solution.java
@@ -0,0 +1,40 @@
+package com.leetcode._0015;
+
+import java.util.Stack;
+
+/*
+206 Reverse Linked List
+Reverse a singly linked list.
+Example:
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+ */
+public class Solution {
+    static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int val){
+            this.val = val;
+        }
+    }
+
+//    public ListNode reverseList(ListNode head) {
+//        if (head==null)return null;
+//        if (head.next==null) return head;
+//        Stack<ListNode> stack = new Stack<>();
+//        ListNode temp = head;
+//        while(temp.next!=null){
+//            stack.push(temp);
+//            temp = temp.next;
+//        }
+//        ListNode newHead = stack.pop();
+//        while (!stack.isEmpty()){
+//            ListNode node = stack.pop();
+//            node.next = node;
+//        }
+//
+//    }
+    public static void main(String[] args) {
+
+    }
+}
