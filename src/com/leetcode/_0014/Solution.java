da9b189698bfcb12af638bcb1e740133ff54079d
chenggang <954036868@qq.com>
2020-03-22 18:00:07 +0800

    -

--- src/com/leetcode/_0014/Solution.java
+++ src/com/leetcode/_0014/Solution.java
@@ -16,7 +16,29 @@ Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (m
 Total amount you can rob = 2 + 9 + 1 = 12.
  */
 public class Solution {
+    //T:O(n) S:O(n)
+    static int rob(int[] nums) {
+        int[] dp=new int[nums.length+1];
+        for(int i=1; i<=nums.length; i++) {
+            if(i==1) dp[i]=nums[i-1];
+            else dp[i]=Math.max(dp[i-1], dp[i-2]+nums[i-1]);
+        }
+        return dp[nums.length];
+    }
+
+    //T:O(n) S:O(1)
+    static int rob2(int[] nums){
+        int dp_pre=0, dp=nums[0];
+        for(int i=1; i<nums.length; i++) {
+            int tmp=dp;
+            dp=Math.max(dp, dp_pre+nums[i]);
+            dp_pre=tmp;
+        }
+        return dp;
+    }
+
     public static void main(String[] args) {
-        System.out.println("====");
+        int[] nums = new int[]{2,7,9,3,1};
+        System.out.println(rob(nums));
     }
 }
