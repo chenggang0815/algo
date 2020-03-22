d14dee156c9c2640713a46147d3ea85833f2be3a
chenggang <954036868@qq.com>
2020-03-23 01:20:42 +0800

    -

--- src/com/leetcode/_0018/Solution.java
+++ src/com/leetcode/_0018/Solution.java
@@ -1,5 +1,6 @@
 package com.leetcode._0018;
 /*
+283. Move Zeroes
 Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
 Example:
