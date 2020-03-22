f64d6b3def274e41a6d9603a2a28b48ec05bc445
chenggang <954036868@qq.com>
2020-03-22 21:55:07 +0800

    283. Move Zeroes

--- src/com/leetcode/_0016/Solution.java
+++ src/com/leetcode/_0016/Solution.java
@@ -10,6 +10,16 @@ public class Solution {
         }
     }
 
+    static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
 
     public static void main(String[] args) {
 
---
+++ src/com/leetcode/_0018/Solution.java
@@ -0,0 +1,33 @@
+package com.leetcode._0018;
+/*
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+    You must do this in-place without making a copy of the array.
+    Minimize the total number of operations.
+ */
+public class Solution {
+    //T:O(n);S:O(1)
+    static void moveZeroes(int[] nums){
+        int length=nums.length;
+        int num=0;
+        for (int item:nums){
+            if (item!=0){
+                nums[num++]=item;
+            }
+        }
+        while (num<length){
+            nums[num++]=0;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,2,3,0,2};
+        moveZeroes(nums);
+        for (int num:nums){
+            System.out.println(num);
+        }
+    }
+}
