031b6b4fe392da724d99f0b832772cb99a6db628
chenggang <954036868@qq.com>
2020-04-07 21:49:24 +0800

    -

--- src/com/leetcode/_0024/Solution.java
+++ src/com/leetcode/_0024/Solution.java
@@ -16,10 +16,10 @@ Output:
 public class Solution {
      static List<List<Integer>> res = new ArrayList<>();
 
-     static List<List<Integer>> subsets(int[] nums) {
-        helper(nums, 0, new ArrayList<>());
-        return res;
-    }
+//     static List<List<Integer>> subsets(int[] nums) {
+//        helper(nums, 0, new ArrayList<>());
+//        return res;
+//    }
 
     // 错误解法 arr变量一直都是同一个，每次会覆盖上次的值
 //    public static void helper(int[] nums, int index, ArrayList<Integer> arr){
@@ -34,23 +34,55 @@ public class Solution {
 //        helper(nums, index+1, arr);
 //    }
 
-    public static void helper(int[] nums, int index, List<Integer> arr){
-        if (index==nums.length){
-            System.out.println(arr);
-            res.add(arr);
-            return;
-        }
-        List<Integer> temparr = new ArrayList<>();
-        for (int i=0;i<arr.size();i++){
-            temparr.add(arr.get(i));
-        }
-        temparr.add(nums[index]);
-        helper(nums, index+1, temparr);
-        helper(nums, index+1, arr);
+//    public static void helper(int[] nums, int index, List<Integer> arr){
+//        if (index==nums.length){
+//            System.out.println(arr);
+//            res.add(arr);
+//            return;
+//        }
+//        List<Integer> temparr = new ArrayList<>();
+//        for (int i=0;i<arr.size();i++){
+//            temparr.add(arr.get(i));
+//        }
+//        temparr.add(nums[index]);
+//        helper(nums, index+1, temparr);
+//        helper(nums, index+1, arr);
+//    }
+
+    static public List<List<Integer>> permute(int[] nums) {
+
+        List<Integer> arr = new ArrayList<>();
+
+        helper(nums, arr);
+
+        return res;
     }
 
+    static public void helper(int[] nums, List<Integer> arr){
+        if(arr.size()==nums.length){
+            System.out.println(arr);
+            res.add(new ArrayList<>(arr));
+            return;
+        }
+        System.out.println("====");
+        for(int i=0;i<nums.length;i++){
+            //System.out.println(nums[i]);
+            if(arr.contains(nums[i])){
+                continue;
+            }
+            List<Integer> temparr = new ArrayList<>();
+            for(int j=0;j<arr.size();j++){
+                temparr.add(arr.get(j));
+            }
+            temparr.add(nums[i]);
+            //System.out.println(temparr);
+            helper(nums, temparr);
+
+            temparr.remove(temparr.size()-1);
+        }
+    }
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3};
-        System.out.println(subsets(nums));
+        System.out.println(permute(nums));
     }
 }
