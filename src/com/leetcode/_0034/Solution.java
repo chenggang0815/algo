43d734a01e85916fcefab97cb60b3be32d965ae2
chenggang <954036868@qq.com>
2020-04-29 16:05:19 +0800

    58. Length of Last Word

--- src/com/leetcode/_0034/Solution.java
+++ src/com/leetcode/_0034/Solution.java
@@ -10,30 +10,43 @@ Input: "Hello World"
 Output: 5
  */
 public class Solution {
+    // time complexity: o(n)
+    //space complexity:(1)
+    //先去掉字符串最右边的" "，再找出第一个出现" "的位置
     static public int lengthOfLastWord(String s) {
         if (s.isEmpty())return 0;
-        char[] c = s.toCharArray();
-        int temp=0;
-        for(int j=0;j<c.length;j++){
-            if(c[j]==' '){
-                temp++;
+        for (int i=s.length()-1;i>=0;i--){
+            if (s.charAt(i)!=' '){
+                s = s.substring(0,i+1);
+                break;
             }
         }
-        if(temp==c.length) return 0;
-
-        for (int i=c.length-1;i>=0;i--){
-            if (c[i]==' '&&i!=c.length-1) return c.length-1-i;
+        //System.out.println("s2="+s);
+        for (int i=s.length()-1;i>=0;i--){
+            if (s.charAt(i)==' '){
+                return s.length()-1-i;
+            }
         }
-        System.out.println("======");
-        for (int i=0;i<c.length;i++){
-            System.out.println(i);
-            if (c[0]!=' '&&i!=0) return i;
-        }
-
-        return c.length;
+        return s.length();
     }
+
+    //每次" "之后都是一个新的单词，从左到右统计每个单词的长度，最后返回的是最后一个单词的长度
+    //o(n)
+    //o(1)
+    static public int lengthOfLastWord2(String s){
+        int res = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) != ' ') {
+                if (i != 0 && s.charAt(i - 1) == ' ') res = 1;
+                else ++res;
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        String s="a ";
+        String s=" a aa ";
+        //String s = "hello world";
         System.out.println(lengthOfLastWord(s));
 
     }
