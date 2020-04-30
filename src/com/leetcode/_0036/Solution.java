054fd288e2be1ecddec9407589f82ae83f9f1c18
chenggang0815 <954036868@qq.com>
2020-04-30 13:56:39 +0800

    -

--- src/com/leetcode/_0036/Solution.java
+++ src/com/leetcode/_0036/Solution.java
@@ -2,7 +2,7 @@ package com.leetcode._0036;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("======");
+        System.out.println("=======");
 
     }
 }
