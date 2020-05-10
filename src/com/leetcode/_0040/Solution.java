dada5cd1247a2e669f3d3aa4c65fc5a7622ada82
chenggang0815 <954036868@qq.com>
2020-05-11 00:07:03 +0800

    100. Same Tree

--- src/com/leetcode/_0040/Solution.java
+++ src/com/leetcode/_0040/Solution.java
@@ -5,7 +5,6 @@ package com.leetcode._0040;
 Given two binary trees, write a function to check if they are the same or not.
 Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
 Example 1:
-
 Input:     1         1
           / \       / \
          2   3     2   3
@@ -15,7 +14,6 @@ Input:     1         1
 Output: true
 
 Example 2:
-
 Input:     1         1
           /           \
          2             2
