6c05e41607246d285d678dd6d08fde17b07871be
chenggang0815 <954036868@qq.com>
2020-05-22 22:50:58 +0800

    knapsack_problem

--- src/com/kernel/dynamic_programming/temporary.java
+++ src/com/kernel/dynamic_programming/temporary.java
@@ -4,75 +4,53 @@ package com.kernel.dynamic_programming;
  */
 public class temporary {
 
-    //1 91. Decode Ways
     /*
-    A message containing letters from A-Z is being encoded to numbers using the following mapping:
-
-'A' -> 1
-'B' -> 2
-...
-'Z' -> 26
-
-Given a non-empty string containing only digits, determine the total number of ways to decode it.
-
 Example 1:
+Input: [5,3,4,5]
+Output: true
+Explanation:
+Alex starts first, and can only take the first 5 or the last 5.
+Say he takes the first 5, so that the row becomes [3, 4, 5].
+If Lee takes 3, then the board is [4, 5], and Alex takes 5 to win with 10 points.
+If Lee takes the last 5, then the board is [3, 4], and Alex takes 4 to win with 9 points.
+This demonstrated that taking the first 5 was a winning move for Alex, so we return true.
 
-Input: "12"
-Output: 2
-Explanation: It could be decoded as "AB" (1 2) or "L" (12).
-
-Example 2:
-
-Input: "226"
-Output: 3
-Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
-
-
+Note:
+    2 <= piles.length <= 500
+    piles.length is even.
+    1 <= piles[i] <= 500
+    sum(piles) is odd.
      */
 
-    static int decode(String s){
-        if (s.length()==0 || s.charAt(0)=='0') return 0;
-        if (s.length()==1) return 1;
-        char[] c = s.toCharArray();
-        int[] dp = new int[c.length+1];
-        dp[0] = 1;
-        int m = (c[0]-'0')*10+c[1]-'0';
-        if (c[1]!='0'&&m<=26){
-            dp[1] = 2;
-        }else if (c[1]!='0'&&m>26){
-            dp[1] = 1;
-        }else if (c[1]=='0'&&m>26){
-            dp[1] = 0;
-        }else if (c[1]=='0'&&m<=26){
-            dp[1] = 1;
+    /*
+    dp[i][j] = max(nums[i]-dp[i+1][j],nums[j]-dp[i][j-1])
+    dp[0][1] dp[1][2] ... dp[n-2][n-1]
+    dp[0][2]              dp[n-3][n-1]
+    return dp[0][n-1] >0
+     */
+    static boolean stonegame(int[] nums){
+        int length = nums.length;
+        int[][] dp = new int[length][length];
+        for (int n=0;n<length;n++){
+            dp[n][n] = nums[n];
         }
 
-        System.out.println(dp[1]);
-
-        for (int i=2;i<c.length;i++){
-            int n = (c[i-1]-'0')*10+c[i]-'0';
-            if (c[i]!='0'&&c[i-1]!='0') {
-                if (n>26) dp[i] = dp[i-1];
-                else dp[i] = dp[i-1]+dp[i-2];
-            }else if (c[i]!='0'&&c[i-1]=='0'){
-                dp[i] = dp[i-1];
-            }else if (c[i]=='0'&&c[i-1]!='0'){
-                if (n>26) return 0;
-                else dp[i] = dp[i-2];
-            }else if (c[i]=='0'&&c[i-1]=='0')
-                return 0;
+        for (int j=1;j<length;j++){
+            for (int i=0;i<length-j;i++){
+                dp[i][i+j] = Math.max(nums[i]-dp[i+1][i+j],nums[i+j]-dp[i][i+j-1]);
+            }
         }
 
-        return dp[c.length-1];
+        System.out.println(dp[0][length-1]);
+
+        return dp[0][length-1]>0;
     }
 
 
     public static void main(String[] args) {
-        String s = "100";
-        String s2 = "101";
-        String s3 = "110";
+        int[] nums = new int[]{5,2,4,5};
+        System.out.println(stonegame(nums));
 
-        System.out.println(decode(s2));
 
     }
 }
