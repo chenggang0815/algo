5f6d7e7f9b421dd9e019526e2f69c344bef4ef01
chenggang0815 <954036868@qq.com>
2020-05-22 10:10:39 +0800

    层次遍历

---
+++ src/com/nowcoder/_031/Solution.java
@@ -0,0 +1,49 @@
+package com.nowcoder._031;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+
+从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+
+ */
+
+
+public class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    static public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        if (root==null) return new ArrayList<>();
+        ArrayList<Integer> array = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int count = queue.size();
+            while (count>0){
+                TreeNode node = queue.poll();
+                array.add(node.val);
+                if (node.left!=null) queue.offer(node.left);
+                if (node.right!=null) queue.offer(node.right);
+                count--;
+            }
+        }
+
+        return array;
+    }
+
+
+        }
+    public static void main(String[] args) {
+
+    }
+}
