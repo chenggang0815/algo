72d3e02b08aeab974cbc8d9a3161f3273be3583a
chenggang0815 <954036868@qq.com>
2020-05-25 09:32:16 +0800

    是否子树

--- src/com/nowcoder/_032/Solution.java
+++ src/com/nowcoder/_032/Solution.java
@@ -35,7 +35,7 @@ public class Solution {
     }
 
     static boolean checkSubTree(TreeNode t1, TreeNode t2) {
-        if (t1==null&&t2==null) return true;
+        if (t1==null&&t2==null) return true;  //当输入t1 t2 都为null时，t2还是t1的子树
         if (t1==null||t2==null) return false;
 
         return (t1.val==t2.val&&checkSubTree(t1.left,t2.left)&&checkSubTree(t1.right,t2.right))||checkSubTree(t1.right,t2)||checkSubTree(t1.left,t2);
