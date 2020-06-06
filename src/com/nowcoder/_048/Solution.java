e92f0227bea0460d14ea7cdfcfe3ca5d9d56a58f
chenggang0815 <954036868@qq.com>
2020-06-06 11:09:29 +0800

    判断该二叉树是否是平衡二叉树

--- src/com/nowcoder/_048/Solution.java
+++ src/com/nowcoder/_048/Solution.java
@@ -14,6 +14,8 @@ import java.util.Queue;
 它是一棵空树或它的左右两个子树的高度差的绝对值不超过1，并且左右两个子树都是一棵平衡二叉树
 
 1. 从根结点开始，对每个结点，遍历求其左右子树的高度是否 > 1
+
+2. 从根结点开始，需要对每个结点从上到下遍历判断，会有重复遍历；因此，可以从叶子结点开始往上遍历，如果不满足条件直接退出
  */
 public class Solution {
     static class TreeNode{
@@ -51,6 +53,35 @@ public class Solution {
 
         return depth;
     }
+
+    //dfs求高度
+    static boolean IsBalanced_Solution2(TreeNode root) {
+        if (root == null) return true;
+
+        return IsBalanced_Solution(root.left) && IsBalanced_Solution(root.right) && (Math.abs(depth(root.left) - depth(root.right)) < 2);
+    }
+
+    static int depth(TreeNode root){
+        return (root == null) ? 0 : Math.max(depth(root.right), depth(root.left)) + 1;
+    }
+
+
+    // 思路2
+    static boolean IsBalanced_Solution3(TreeNode root) {
+        return getDepth(root) != -1;
+    }
+
+    static int getDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = getDepth(root.left);
+        if (left == -1) return -1;
+        int right = getDepth(root.right);
+        if (right == -1) return -1;
+
+        return Math.abs(left - right) > 1 ? -1 : 1 + Math.max(left, right);
+    }
+
     public static void main(String[] args) {
 
     }
