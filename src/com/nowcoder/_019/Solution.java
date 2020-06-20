9217aaac4c1cc03dccabd0977b83aaf1643b3457
chenggang0815 <954036868@qq.com>
2020-06-20 10:36:37 +0800

    mergeSort

--- src/com/nowcoder/_019/Solution.java
+++ src/com/nowcoder/_019/Solution.java
@@ -80,22 +80,29 @@ class Solution {
 //
 //    }
 	// 归并排序
-    private static void  mergeSort(int[] nums, int left, int right){
+    /*
+    归并排序： 时间复杂度： o(nlogn) 空间复杂度：o(n) => 需要将temp数组放在merge函数外面，避免每次递归的时候重新新建一个
+     */
+    static void  mergeSort(int[] nums){
+        int[] temp = new int[nums.length];
+        mergeSort(nums, 0, nums.length - 1, temp);
+    }
+    static void  mergeSort(int[] nums, int left, int right, int[] temp){
         if (left >= right) return;
 
         int mid = left + (right - left) / 2;
-        mergeSort(nums, left, mid);
-        mergeSort(nums,mid+1, right);
+        mergeSort(nums, left, mid, temp);
+        mergeSort(nums,mid+1, right, temp);
 
-        merge(nums, left, mid, right);
+        merge(nums, left, mid, right, temp);
 
     }
 
-    static void merge(int[] nums, int left, int mid, int right){
+    static void merge(int[] nums, int left, int mid, int right, int[] temp){
         int i = left;
         int j = mid + 1;
-        int[] temp = new int[right - left + 1];
-        int t = 0;
+        //int[] temp = new int[right - left + 1];
+        int t = i;
         while (i <= mid && j <= right){
             if (nums[i] <= nums[j]){
                 temp[t++] = nums[i++];
@@ -111,8 +118,8 @@ class Solution {
             temp[t++] = nums[j++];
         }
 
-        for (i = 0;i < t;i++){
-            nums[i + left] = temp[i];
+        for (i = left;i < t;i++){
+            nums[i] = temp[i];
         }
     }
 
@@ -120,8 +127,8 @@ class Solution {
 
         //int[] array = new int[] {40,2,1,311,2,1};
         int[] nums = {10,7,2,4,7,62,3,4,2,1,8,9,19};
-        //mergeSort(nums,0,nums.length-1);
-        quickSort(nums,0,nums.length-1);
+        mergeSort(nums);
+       // quickSort(nums,0,nums.length-1);
         System.out.println(Arrays.toString(nums));
         
 
