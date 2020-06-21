e38ad558dc8eb85fe1de58b7eb0f1e69cd731bbc
chenggang0815 <954036868@qq.com>
2020-06-21 17:00:46 +0800

    字符串的排列

--- src/com/nowcoder/_071/Solution.java
+++ src/com/nowcoder/_071/Solution.java
@@ -1,14 +1,19 @@
 package com.nowcoder._071;
 /*
 字符串的排列
+输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
 
+思路： 回溯算法
+1. 假设有abc ， 依次固定 a b c， 再分别从index=[0,nums.length-1];i=[index,nums.length-1];
+2. 两两交换 swap(index, i)
+3. 对于当前固定的元素，如果在hashset中，则跳过，避免重复计算
  */
 import java.util.*;
 
 public class Solution {
     static ArrayList<String> array = new ArrayList<>();
     static char[] c;
-
+    //time: O(N!) space: o(N^2) 递归深度为N，且每次递归都需要创建hashset
     static ArrayList<String> Permutation(String str) {
         if (str.length() == 0) return new ArrayList<>();
 
