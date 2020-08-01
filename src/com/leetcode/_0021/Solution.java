aa24e39f529eb111e814b1f857536d2fda72009c
chenggang0815 <954036868@qq.com>
2020-08-01 22:33:23 +0800

    二叉树中任意两个结点路径长度中的最大值

--- src/com/leetcode/_0021/Solution.java
+++ src/com/leetcode/_0021/Solution.java
@@ -1,6 +1,6 @@
 package com.leetcode._0021;
 /*
-543. Diameter of Binary Tree  Easy 
+543. Diameter of Binary Tree  Easy
 Given a binary tree, you need to compute the length of the diameter of the tree.
 The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
 This path may or may not pass through the root.
