e1c3e6a0d9a439c1c5ff7b0a6ee66c0b4073c578
chenggang0815 <954036868@qq.com>
2020-08-01 15:47:02 +0800

    二叉树中任意两个结点路径长度中的最大值

--- src/com/leetcode/_0021/Solution.java
+++ src/com/leetcode/_0021/Solution.java
@@ -1,7 +1,9 @@
 package com.leetcode._0021;
 /*
-543. Diameter of Binary Tree  Easy
-Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+543. Diameter of Binary Tree  Easy 
+Given a binary tree, you need to compute the length of the diameter of the tree.
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
+This path may or may not pass through the root.
 
 Example:
 Given a binary tree
@@ -33,21 +35,25 @@ public class Solution {
             preOrder(root.right);
         }
     }
-
-    static int depth(TreeNode root){
-        if (root==null) return 0;
-        int left = depth(root.left);
-        int right = depth(root.right);
-        maxPath = Math.max(maxPath,left+right);
-        return Math.max(left,right)+1;
-    }
-
     static int maxPath = 0;
     static int Diameter(TreeNode root){
-        if (root==null) return 0;
+        if (root == null) return 0;
+
         depth(root);
+
         return maxPath;
     }
+
+    static int depth(TreeNode root){
+        if (root == null) return 0;
+
+        int left = depth(root.left);
+        int right = depth(root.right);
+        maxPath = Math.max(maxPath, left + right); //对于每个结点都要计算经过它的左右最大路径和
+
+        return Math.max(left, right) + 1;
+    }
+
 
 
     public static void main(String[] args) {
@@ -56,14 +62,17 @@ public class Solution {
         TreeNode node2 = new TreeNode(3);
         TreeNode node3 = new TreeNode(4);
         TreeNode node4 = new TreeNode(5);
+        TreeNode node5 = new TreeNode(6);
         root.left = node1;
         root.right = node2;
         node1.left = node3;
         node1.right = node4;
+        node4.left = node5;
         //preOrder(root);
-//        System.out.println(depth(root.right));
-//        System.out.println(depth(root.left));
+        //System.out.println(depth(root.right));
+        //System.out.println(depth(root.left));
         System.out.println(Diameter(root));
+        //System.out.println(depth(root));
 
 
 
