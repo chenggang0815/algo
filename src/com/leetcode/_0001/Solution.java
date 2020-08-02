92345d98d9ef597494a2bcb9c27c445a75919c25
chenggang0815 <954036868@qq.com>
2020-08-02 18:47:21 +0800

    两数之和

--- src/com/leetcode/_0001/Solution.java
+++ src/com/leetcode/_0001/Solution.java
@@ -1,59 +1,59 @@
 package com.leetcode._0001;
-import java.util.Arrays;
 import java.util.HashMap;
 
-/*Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+/*
+1. Two Sum
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-        You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-        Example:
-
-        Given nums = [2, 7, 11, 15], target = 9,
-
-        Because nums[0] + nums[1] = 2 + 7 = 9,
-        return [0, 1].
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
 */
 public class Solution {
     //  Brute Force
-    //Time complexity : O(n)
+    //Time complexity : O(n^2) space:o(1)
     public static int[] twoSum(int[] nums, int target) {
-            for(int i = 0; i< nums.length-1; i++){
-                for(int j = i+1; j < nums.length; j++){
+            for(int i = 0; i< nums.length - 1; i++){
+                for(int j = i + 1; j < nums.length; j++){
                     if(nums[i] + nums[j] == target){
-                        int[] arr = new int[]{i,j};
+                        int[] arr = new int[]{i, j};
                         return arr;
                     }
                 }
             }
+
             int[] arr = new int[]{};
+
             return arr;
     }
 
     //Time complexity : O(n)
-    //Space complexity :O(n). The extra space required depends on the number of items stored in the hash table, which stores exactly nnn elements.
+    //Space complexity :O(n)
     public static int[] twoSum2(int[] nums, int target){
-        HashMap<Integer,Integer> map= new HashMap<>();
-        for (int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
-        }
-
-        for (int i=0;i<nums.length;i++){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
             int value = target - nums[i];
-            if (map.containsKey(value) && map.get(value)!=i){
-                return new int[]{map.get(value),i};
+            if (map.containsKey(value) && map.get(value) != i){
+                return new int[]{map.get(value), i};
             }
         }
+
         return new int[]{};
     }
 
+    //Time complexity : O(n)
+    //Space complexity :O(n)
     public static int[] twoSum3(int[] nums, int target) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0;i<nums.length;i++){
+        for(int i = 0; i<nums.length; i++){
             int value = target - nums[i];
-            if(map.containsKey(value) && map.get(value)!=i){
+            if(map.containsKey(value)){
                 return new int[]{map.get(value),i};
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
 
         return new int[]{};
@@ -61,7 +61,7 @@ public class Solution {
 
 
     public static void main(String[] args) {
-        int[] array = new int[]{1,2,3,4};
-        System.out.println(Arrays.toString(twoSum2(array,7)));
+        int[] array = new int[]{0,2,3,4};
+        System.out.println(Arrays.toString(twoSum3(array,0)));
     }
 }
--- src/com/leetcode/_0013/Solution.java
+++ src/com/leetcode/_0013/Solution.java
@@ -32,7 +32,7 @@ public class Solution {
 
         return -1;
     }
-    
+
     public static void main(String[] args) {
 
         int[] nums = new int[]{2,2,1,1,1,2,2};
