67d002f4978a45236ae5e886b21452430a8fac12
chenggang0815 <954036868@qq.com>
2020-08-02 20:08:55 +0800

    删除排序数组中的重复项

--- src/com/leetcode/_0029/Solution.java
+++ src/com/leetcode/_0029/Solution.java
@@ -1,8 +1,14 @@
 package com.leetcode._0029;
-/*
-26. Remove Duplicates from Sorted Array Easy
 
-Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+import java.util.Arrays;
+
+/*
+26. Remove Duplicates from Sorted Array Easy 删除排序数组中的重复项
+
+给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在原地修改输入数组 并在使用O(1)额外空间的条件下完成。
+
 
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
@@ -38,28 +44,34 @@ int len = removeDuplicates(nums);
 for (int i = 0; i < len; i++) {
     print(nums[i]);
 }
+
+思路：
+
+1. 双指针，nums[right] != nums[left] 时， left++(left右移一位)，将nums[right]赋值给nums[right]， right++
+2. 最后nums中不重复的数字有left + 1 个 [0, left + 1]
+
  */
 public class Solution {
 
     // time complexity: o(n)
     // space complexity:o(1)
     static public int removeDuplicates(int[] nums) {
-        int index = 0;
-        for (int i=1;i<nums.length;i++){
-            if (nums[i]!=nums[index]){
-                index++;
-                nums[index] = nums[i];
+        int left = 0;
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] != nums[left]){
+                left++;
+                nums[left] = nums[i];
             }
         }
 
-        return index+1;
+        return left + 1;
     }
     public static void main(String[] args) {
         int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
         int length = removeDuplicates(nums);
+        System.out.println(length);
         for (int i=0;i<length;i++){
             System.out.println(nums[i]);
         }
-
     }
 }
