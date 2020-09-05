bf1f7882d0a7cb5643b8f135405615e04c66f84e
chenggang0815 <954036868@qq.com>
2020-09-05 18:26:32 +0800

    -

---
+++ src/com/leetcode/_0025_K个一组翻转链表/Solution.java
@@ -0,0 +1,19 @@
+package com.leetcode._0025_K个一组翻转链表;
+
+public class Solution {
+    static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int val){
+            this.val = val;
+        }
+    }
+
+//    static ListNode reverseKGroup(ListNode head, int k){
+//
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
