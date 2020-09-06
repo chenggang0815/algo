6f19b6a4ac7a891121eea8266738029cc7076047
chenggang0815 <954036868@qq.com>
2020-09-06 14:58:39 +0800

    合并两个有序链表

--- src/com/LeetCode/_0021_合并两个有序链表/Solution.java
+++ src/com/LeetCode/_0021_合并两个有序链表/Solution.java
@@ -1,5 +1,6 @@
 package com.LeetCode._0021_合并两个有序链表;
 /*
+合并两个有序链表
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 
 Example:
@@ -18,8 +19,6 @@ public class Solution {
         }
     }
 
-    static ListNode head1 = new ListNode();
-    static ListNode head2 = new ListNode();
 
     static void addNode(ListNode head, int val){
         ListNode newNode = new ListNode(val);
@@ -43,17 +42,13 @@ public class Solution {
 
     // Time Complexity:O(n)
     // Space Complexity:O(1)
-    static ListNode mergeTwoList(ListNode l1, ListNode l2){
-        if (l1==null && l2==null){
-            return null;
-        }else if (l1==null && l2 !=null)return l2;
-        else if (l2==null && l1!=null)return l1;
+    static ListNode mergeTwoList(ListNode head1, ListNode head2){
 
         ListNode temp1 = head1;
         ListNode temp2 = head2;
         ListNode newHead = new ListNode();
         ListNode temp = newHead;
-        while (temp1!=null && temp2!=null){
+        while (temp1 != null && temp2 != null){
             if (temp1.val < temp2.val){
                 temp.next = temp1;
                 temp1 = temp1.next;
@@ -65,10 +60,10 @@ public class Solution {
             temp = temp.next;
         }
 
-        if (temp1==null){
+        if (temp1 == null){
             temp.next=temp2;
         }
-        if (temp2==null){
+        if (temp2 == null){
             temp.next=temp1;
         }
         return newHead;
@@ -89,6 +84,8 @@ public class Solution {
     }
     
     public static void main(String[] args) {
+        ListNode head1 = new ListNode(0);
+        ListNode head2 = new ListNode(0);
         addNode(head1,1);
         addNode(head1,2);
         addNode(head1,4);
