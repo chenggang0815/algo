bea415494b0f95ccd09e1f012c7569ba4d64e748
chenggang0815 <954036868@qq.com>
2020-09-26 21:01:23 +0800

    下一个排列

---
+++ src/com/LeetCode/_0662_二叉树的最大宽度/Solution.java
@@ -0,0 +1,15 @@
+package com.LeetCode._0662_二叉树的最大宽度;
+/*
+662. 二叉树最大宽度
+
+给定一个二叉树，编写一个函数来获取这个树的最大宽度。树的宽度是所有层中的最大宽度。
+这个二叉树与满二叉树（full binary tree）结构相同，但一些节点为空。
+
+每一层的宽度被定义为:该层最左和最右的非空节点（两端点间的null节点也计入长度）之间的长度。
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+}
