87029a24b440681d3b0677e39d7f96f38c6ef011
chenggang <954036868@qq.com>
2021-01-06 19:45:38 +0800

    Longest_Substring_Without_Repeating_Characters

--- src/com/LeetCode/_0003_无重复字符的最长子串/Solution.java
+++ src/com/LeetCode/_0003_无重复字符的最长子串/Solution.java
@@ -31,8 +31,9 @@ public class Solution {
 
         return res;
     }
+
     public static void main(String[] args) {
-        System.out.println(longestSubString("abcabcbb"));
+        System.out.println(longestSubString("abcabefgh"));
     }
 }
 
