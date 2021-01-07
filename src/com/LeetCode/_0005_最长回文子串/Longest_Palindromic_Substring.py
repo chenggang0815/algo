d48563a18031c6c29f4c440c9d7280e7391e6cce
chenggang <954036868@qq.com>
2021-01-07 20:36:41 +0800

    -

---
+++ src/com/LeetCode/_0005_最长回文子串/Longest_Palindromic_Substring.py
@@ -0,0 +1,16 @@
+"""
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+"""
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
