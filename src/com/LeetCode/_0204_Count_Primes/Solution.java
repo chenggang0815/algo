24bf15e4d8f0766ff1294580a78403a1a3436b86
chenggang <954036868@qq.com>
2021-01-13 19:22:48 +0800

    204. Count Primes

---
+++ src/com/LeetCode/_0204_Count_Primes/Solution.java
@@ -0,0 +1,27 @@
+package com.LeetCode._0204_Count_Primes;
+/*
+204. Count Primes
+
+Example 1:
+Input: n = 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+Example 2:
+Input: n = 0
+Output: 0
+
+Example 3:
+Input: n = 1
+Output: 0
+ */
+public class Solution {
+    
+    static int countPrimes(int n) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
