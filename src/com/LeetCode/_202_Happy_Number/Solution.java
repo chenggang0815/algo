bf181f0ff2c5dab52c3656b874ca3596bd514668
chenggang <954036868@qq.com>
2021-01-14 17:34:10 +0800

    _202_Happy_Number

---
+++ src/com/LeetCode/_202_Happy_Number/Solution.java
@@ -0,0 +1,25 @@
+package com.LeetCode._202_Happy_Number;
+
+import java.util.HashSet;
+/*
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+1^2 + 9^2 = 82
+8^2 + 2^2 = 68
+6^2 + 8^2 = 100
+1^2 + 0^2 + 02 = 1
+ */
+public class Solution {
+
+    static boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
