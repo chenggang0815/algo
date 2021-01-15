d441c787afbc6b7b3ff3f6d4b63312a033aba820
chenggang <954036868@qq.com>
2021-01-15 18:42:26 +0800

    _0039_Combination_Sum

---
+++ src/com/LeetCode/_0015_3Sum/Solution.java
@@ -0,0 +1,22 @@
+package com.LeetCode._0015_3Sum;
+/*
+15. 3Sum
+
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+ */
+
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+    }
+        public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/com/LeetCode/_0039_Combination_Sum/Solution.java
@@ -0,0 +1,9 @@
+package com.LeetCode._0039_Combination_Sum;
+/*
+
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
