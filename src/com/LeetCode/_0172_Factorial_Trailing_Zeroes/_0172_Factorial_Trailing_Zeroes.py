06f5f618b6e1c3b8a4dc882b2d5850b400a2f51c
chenggang0815 <954036868@qq.com>
2021-01-20 22:14:25 +0800

    _0172_Factorial_Trailing_Zeroes

--- src/com/LeetCode/_0172_Factorial_Trailing_Zeroes/_0172_Factorial_Trailing_Zeroes.py
+++ src/com/LeetCode/_0172_Factorial_Trailing_Zeroes/_0172_Factorial_Trailing_Zeroes.py
@@ -1,18 +1,26 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        def numZero(n):
+        def numFive(n):
             res = 0
-            while n % 10 == 0:
+            while n % 5 == 0:
                 res += 1
-                n //= 10
+                n //= 5
             return res
         ans = 1
-        for i in range(n, 0, -1):
+        for i in range(n, 0, -5):
             ans = ans * i
-        return numZero(ans)
+        return numFive(ans)
+
+    def trailingZeroes1(self, n: int) -> int:
+        res = 0
+        while n > 0:
+            res += n // 5
+            n //= 5
+        return res
+
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.trailingZeroes(60))
+    print(s.trailingZeroes1(125))
 
