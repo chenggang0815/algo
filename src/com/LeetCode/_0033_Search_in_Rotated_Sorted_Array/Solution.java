9f3806a92a94b282e1baf9aeff7075915abb5884
chenggang <954036868@qq.com>
2021-01-22 12:24:00 +0800

    _0033_Search_in_Rotated_Sorted_Array

--- src/com/LeetCode/_0033_Search_in_Rotated_Sorted_Array/Solution.java
+++ src/com/LeetCode/_0033_Search_in_Rotated_Sorted_Array/Solution.java
@@ -22,33 +22,50 @@ Example 3:
 Input: nums = [1], target = 0
 Output: -1
  */
+
+/*
+思路一：
+1. 将数组从中间分开成左右两部分的时候，一定有一部分的数组是有序的。
+拿[4,5,6,7,0,1,2]来看，从6这个位置分开以后数组变成了[4,5,6]和[7,0,1,2]两个部分，其中左边[4,5,6]这个部分的数组是有序的，其他也是如此。
+
+2. 这启示我们可以在常规二分搜索的时候查看当前mid为分割位置分割出来的两个部分[l, mid]和[mid + 1, r]哪个部分是有序的，
+
+3. 并根据有序的那个部分确定我们该如何改变二分搜索的上下界 => 因为我们能够根据有序的那部分判断出target在不在这个部分
+
+    3.1 如果[l,mid-1]是有序数组，且target满足nums[left] <= target && target < nums[mid]，则应该将搜索范围缩小至[left, mid-1]，否则在[mid+1,right]中寻找
+    3.2 如果[mid, r]是有序数组，且target满足nums[mid] < target && target <= nums[right]，则应该将搜索范围缩小至[mid+1,right]，否则在[left, mid-1]中寻找
+
+time: o(log(n))
+
+
+ */
 public class Solution {
-    static int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
-
-        while (start <= end){
-            int mid = start + (end-start)/2;
+    // 思路1
+    static int search(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) return mid;
-            else if (nums[mid]>= nums[start]){
-                if (target >= nums[start] && target < nums[mid]){
-                    end = mid-1;
+            if (nums[0] <= nums[mid]){ //注意细节 =>  nums[0] <= nums[mid] => [3,1] 1
+                if (nums[left] <= target && target < nums[mid]){ //注意细节 =>  nums[left] <= target => [1,3,5] 1
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
                 }
-                else start = mid +1;
-            }
-            else if (nums[mid]<= nums[end]){
-                if(target > nums[mid] && target <= nums[end]){
-                    start =mid+1;
+            }else{
+                if(nums[mid] < target && target <= nums[right]){ //注意细节 =>  nums[left] <= target => [6,3,5] 5
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
                 }
-                else end = mid -1;
-
             }
         }
 
         return -1;
-        }
+    }
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(search(new int[]{3,1}, 3));
     }
 }
