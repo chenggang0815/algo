dfddf06900b46fec8e190083622ec7331b2b982f
chenggang <954036868@qq.com>
2021-01-25 20:34:14 +0800

    _0055_Jump_Game

---
+++ src/com/LeetCode/_0055_Jump_Game/Solution.java
@@ -0,0 +1,26 @@
+package com.LeetCode._0055_Jump_Game;
+/*
+55. Jump Game
+
+Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+
+Example 1:
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ */
+public class Solution {
+    public boolean canJump(int[] nums) {
+
+    }
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
