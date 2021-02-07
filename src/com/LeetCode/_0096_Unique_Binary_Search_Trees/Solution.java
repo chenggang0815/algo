2ba970e808f6ecb81279c577c5bb84c65eb30005
chenggang <954036868@qq.com>
2021-02-07 20:43:01 +0800

    _0096_Unique_Binary_Search_Trees

---
+++ src/com/LeetCode/_0096_Unique_Binary_Search_Trees/Solution.java
@@ -0,0 +1,12 @@
+package com.LeetCode._0096_Unique_Binary_Search_Trees;
+/*
+96. Unique Binary Search Trees
+
+Given an integer n, return the number of structurally unique BST's (binary search trees)
+which has exactly n nodes of unique values from 1 to n.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
