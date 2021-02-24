b637e4a29a5aabb2fa5c15b1bd76d7b33dc25857
chenggang <954036868@qq.com>
2021-02-24 20:02:49 +0800

    -

--- src/com/LeetCode/_0215_Kth_Largest_Element_in_an_Array/Solution.java
+++ src/com/LeetCode/_0215_Kth_Largest_Element_in_an_Array/Solution.java
@@ -1,12 +1,27 @@
-package com.LeetCode._0215_数组中的第K个最大元素;
+package com.LeetCode._0215_Kth_Largest_Element_in_an_Array;
 /*
 215 - kth largest element in an array
-215 - 数组中的第K个最大元素
-在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
-输入: [3,2,1,5,6,4] 和 k = 2
-输出: 5
+Example 1:
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+
+Example 2:
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+ */
+
+/*
+思路1：
+
+思路2：
+
  */
 public class Solution {
+
+    public static void main(String[] args) {
+
+    }
 
 }
