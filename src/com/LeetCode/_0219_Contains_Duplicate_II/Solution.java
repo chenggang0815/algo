f3acf327ed6076c01d2ab8e51029a78e306e94bd
chenggang0815 <954036868@qq.com>
2021-02-27 22:05:57 +0800

    _0219_Contains_Duplicate_II

---
+++ src/com/LeetCode/_0219_Contains_Duplicate_II/Solution.java
@@ -0,0 +1,40 @@
+package com.LeetCode._0219_Contains_Duplicate_II;
+
+import java.util.HashMap;
+
+/*
+219. Contains Duplicate II
+给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值 至多为 k
+示例 1:
+输入: nums = [1,2,3,1], k = 3
+输出: true
+
+示例 2:
+输入: nums = [1,0,1,1], k = 1
+输出: true
+
+示例 3:
+输入: nums = [1,2,3,1,2,3], k = 2
+输出: false
+ */
+public class Solution {
+    // 一次success
+    static boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            int num = nums[i];
+            if (map.containsKey(num) && i - map.get(num) <= k){
+                return true;
+            }
+            map.put(num, i);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,1,2,3};
+        int k = 2;
+        System.out.println(containsNearbyDuplicate(nums, k));
+    }
+}
