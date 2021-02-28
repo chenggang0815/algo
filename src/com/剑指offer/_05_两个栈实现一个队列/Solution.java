806190c78723ad8fca70c99fb68262c66c660873
chenggang0815 <954036868@qq.com>
2021-02-28 16:18:42 +0800

    _0232_Implement_Queue_using_Stacks

--- src/com/剑指offer/_05_两个栈实现一个队列/Solution.java
+++ src/com/剑指offer/_05_两个栈实现一个队列/Solution.java
@@ -24,14 +24,15 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution s =new Solution();
+        s.push(1);
         s.push(2);
+        s.push(3);
+        System.out.println(""+s.pop());
         s.push(4);
-        s.push(8);
-
         System.out.println(""+s.pop());
         System.out.println(""+s.pop());
         System.out.println(""+s.pop());
-        System.out.println(""+s.pop());
+        // 正确出队列顺序 1，2,3,4
 
     }
 }
