9c23768efebc6571775e6a41243a257ac985853a
chenggang <954036868@qq.com>
2021-03-02 15:07:09 +0800

    _0406_Queue_Reconstruction_by_Height

---
+++ src/com/LeetCode/_0406_Queue_Reconstruction_by_Height/Solution.java
@@ -0,0 +1,20 @@
+package com.LeetCode._0406_Queue_Reconstruction_by_Height;
+/*
+406. Queue Reconstruction by Height
+
+假设有打乱顺序的一群人站成一个队列，数组 people 表示队列中一些人的属性（不一定按顺序）。
+每个people[i] = [hi, ki] 表示第i个人的身高为hi，前面正好有ki个身高大于或等于hi的人。
+
+请你重新构造并返回输入数组 people 所表示的队列。
+返回的队列应该格式化为数组 queue ，其中queue[j]=[hj, kj]是队列中第j个人的属性（queue[0]是排在队列前面的人）
+
+输入：people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+输出：[[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+}
