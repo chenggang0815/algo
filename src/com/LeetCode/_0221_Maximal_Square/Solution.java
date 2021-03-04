2029c1c562dc6500f491970019afa3822e8277be
chenggang0815 <954036868@qq.com>
2021-03-04 23:49:52 +0800

    -

---
+++ src/com/LeetCode/_0221_Maximal_Square/Solution.java
@@ -0,0 +1,22 @@
+package com.LeetCode._0221_Maximal_Square;
+/*
+221. Maximal Square
+
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+Input: matrix = [["1","0","1","0","0"],
+                 ["1","0","1","1","1"],
+                 ["1","1","1","1","1"],
+                 ["1","0","0","1","0"]]
+Output: 4
+
+ */
+public class Solution {
+    
+    static int maximalSquare(char[][] matrix) {
+
+    }
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/com/LeetCode/_0494_Target_Sum/Solution.java
@@ -0,0 +1,26 @@
+package com.LeetCode._0494_Target_Sum;
+/*
+494. Target Sum
+You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+Example 1:
+Input: nums is [1, 1, 1, 1, 1], S is 3.
+Output: 5
+Explanation:
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+
+There are 5 ways to assign symbols to make the sum of nums be target 3.
+ */
+public class Solution {
+    static int findTargetSumWays(int[] nums, int S) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
