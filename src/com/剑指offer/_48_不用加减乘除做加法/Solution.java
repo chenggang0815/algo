6abbd4e2487db28a0aedd1f850983d5218b5e90d
chenggang0815 <954036868@qq.com>
2021-03-25 22:44:38 +0800

    _0371_Sum_of_Two_Integers

--- src/com/剑指offer/_48_不用加减乘除做加法/Solution.java
+++ src/com/剑指offer/_48_不用加减乘除做加法/Solution.java
@@ -1,6 +1,18 @@
 package com.剑指offer._48_不用加减乘除做加法;
 /*
 不用加减乘除做加法
+leetcode 371. Sum of Two Integers
+
+eg 13 + 9 = 2
+13的二进制：1101 9的二进制：1001
+第一轮
+1. 计算不进位的和 => 1101 ^ 1001 => 0100
+2. 计算进位 => 1101 & 1001 => 1001 <<1 => 10010
+第二轮
+1. 计算不进位的和 00100 ^ 10010 => 10110
+2. 计算进位 => 00100 & 10010 => 00000 <<1 => 000000
+
+进位为0，return 10110 => 2^4+2^2+2^1=16+4+2=22
 
 思路：
 1）分析上面对二进制的计算过程，不难发现：
@@ -14,7 +26,6 @@ package com.剑指offer._48_不用加减乘除做加法;
 
 链接：https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/solution/er-jin-zhi-qiu-he-chao-xiang-xi-da-bai-10000yong-h/
 
-
  tip：
  在计算机系统中，数值一律用补码来表示和存储。
  补码的优势： 加法、减法可以统一处理（CPU只有加法器）。因此，以上方法同时适用于正数和负数的加法
@@ -24,7 +35,7 @@ public class Solution {
     //空间复杂度 O(1)
     static public int Add(int num1,int num2) {
         while (num2 != 0){
-            int temp = (num1 ^ num2);
+            int temp = (num1 ^ num2); //求和（不计进位）. 相同位置0，相反位置1
             num2 = (num1 & num2) << 1;
             num1 = temp;
         }
