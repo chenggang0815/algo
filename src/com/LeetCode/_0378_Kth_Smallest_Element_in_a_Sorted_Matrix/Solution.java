1e0d8ce752734b8346c5547f0296d362987dd939
chenggang0815 <954036868@qq.com>
2021-03-25 23:05:35 +0800

    _0378_Kth_Smallest_Element_in_a_Sorted_Matrix

---
+++ src/com/LeetCode/_0378_Kth_Smallest_Element_in_a_Sorted_Matrix/Solution.java
@@ -0,0 +1,35 @@
+package com.LeetCode._0378_Kth_Smallest_Element_in_a_Sorted_Matrix;
+/*
+378. Kth Smallest Element in a Sorted Matrix
+
+Given an n x n matrix where each of the rows and columns are sorted in ascending order, return the kth smallest element in the matrix.
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+Example 1:
+Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+Output: 13
+Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+
+Example 2:
+Input: matrix = [[-5]], k = 1
+Output: -5
+
+Constraints:
+1. n == matrix.length
+2. n == matrix[i].length
+3. 1 <= n <= 300
+4. -109 <= matrix[i][j] <= -109
+5. All the rows and columns of matrix are guaranteed to be sorted in non-degreasing order.
+6. 1 <= k <= n^2
+*/
+
+/*
+[[1,5,9],
+ [10,11,13],
+ [12,13,15]]
+*/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
