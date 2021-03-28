a7f17351ac549d4992eee157db52851b48421cab
chenggang0815 <954036868@qq.com>
2021-03-29 00:10:11 +0800

    _0395_Longest_Substring_with_At_Least_K_Repeating_Characters

---
+++ src/com/LeetCode/_0395_Longest_Substring_with_At_Least_K_Repeating_Characters/Solution.java
@@ -0,0 +1,26 @@
+package com.LeetCode._0395_Longest_Substring_with_At_Least_K_Repeating_Characters;
+/*
+395. Longest Substring with At Least K Repeating Characters
+
+Given a string s and an integer k, return the length of the longest substring of s such that the frequency of each character in this substring is greater than or equal to k.
+
+Example 1:
+Input: s = "aaabb", k = 3
+Output: 3
+Explanation: The longest substring is "aaa", as 'a' is repeated 3 times.
+
+Example 2:
+Input: s = "ababbc", k = 2
+Output: 5
+Explanation: The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
+
+Constraints:
+1 <= s.length <= 104
+s consists of only lowercase English letters.
+1 <= k <= 105
+*/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
