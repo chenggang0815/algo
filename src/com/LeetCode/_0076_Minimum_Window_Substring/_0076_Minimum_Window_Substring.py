7bd1b1d129c0874fb827ad5d12c4b80aec74a733
chenggang0815 <954036868@qq.com>
2021-08-12 22:06:50 +0800

    _0076_Minimum_Window_Substring

--- src/com/LeetCode/_0076_Minimum_Window_Substring/_0076_Minimum_Window_Substring.py
+++ src/com/LeetCode/_0076_Minimum_Window_Substring/_0076_Minimum_Window_Substring.py
@@ -1,4 +1,19 @@
+"""
+76. Minimum Window Substring
+Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+The testcases will be generated such that the answer is unique.
+A substring is a contiguous sequence of characters within the string.
+
+
+Example 1:
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+"""
+
 from collections import Counter
+
+
 class Solution:
     def minWindow1(self, s: str, t: str) -> str:
         if len(t) > len(s):
@@ -8,7 +23,7 @@ class Solution:
         c = Counter(t)
         for i in range(len(s)):
             for j in range(i, len(s)):
-                c1 = Counter(s[i : j + 1])
+                c1 = Counter(s[i: j + 1])
                 count = 0
                 for char in c:
                     if c[char] <= c1[char]:
@@ -21,9 +36,8 @@ class Solution:
                     print("===")
                     print(inedx)
 
-        #print(inedx)
+        # print(inedx)
         return s[inedx[0]:inedx[1]]
-
 
     def minWindow2(self, s: str, t: str) -> str:
         left = 0
@@ -47,9 +61,7 @@ class Solution:
             else:
                 right += 1
 
-
         return s[inedx[0]:inedx[1]]
-
 
     def minWindow3(self, s: str, t: str) -> str:
         def helper1(c1, c2, t):
@@ -57,11 +69,13 @@ class Solution:
                 if c1[char] > c2[char]:
                     return False
             return True
+
         def helper2(c1, c2, t):
             for char in t:
                 if c1[char] != c2[char]:
                     return False
             return True
+
         left = 0
         right = 0
         c = Counter(t)
@@ -83,11 +97,9 @@ class Solution:
             else:
                 right += 1
 
-
         return s[inedx[0]:inedx[1]]
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.minWindow3("acbbaca", "aba"))
-
+    print(s.minWindow2("acbbaca", "aba"))
