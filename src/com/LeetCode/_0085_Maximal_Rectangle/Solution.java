70c21c80ca502ca5dccde711db2560802190a133
chenggang0815 <954036868@qq.com>
2021-09-03 12:41:13 -0400

    _0503_Next_Greater_Element_II

---
+++ src/com/LeetCode/_0085_Maximal_Rectangle/Solution.java
@@ -0,0 +1,19 @@
+package com.LeetCode._0085_Maximal_Rectangle;
+/*
+85. Maximal Rectangle
+Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+
+Example 1:
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 6
+Explanation: The maximal rectangle is shown in the above picture.
+*/
+public class Solution {
+    static int maximalRectangle(char[][] matrix){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
