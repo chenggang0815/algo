7eaf1c8f9fc6e671b098ca24767624c229d44e74
chenggang0815 <954036868@qq.com>
2021-09-20 00:28:25 -0400

    1192. Critical Connections in a Network

---
+++ Amazon/_0023_Merge_k_Sorted_Lists/Solution.java
@@ -0,0 +1,7 @@
+package Amazon._0023_Merge_k_Sorted_Lists;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Amazon/_0692_Top_K_Frequent_Words/Solution.java
@@ -0,0 +1,7 @@
+package Amazon._0692_Top_K_Frequent_Words;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
