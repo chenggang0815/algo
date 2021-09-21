cc66980c2da260a627e799befa85d94f9a47680d
chenggang0815 <954036868@qq.com>
2021-09-20 23:52:29 -0400

    -

---
+++ Amazon/_1152_Analyze_User_Website_Visit_Pattern/Solution.java
@@ -0,0 +1,9 @@
+package Amazon._1152_Analyze_User_Website_Visit_Pattern;
+/*
+
+*/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Amazon/_1167_Minimum_Cost_to_Connect_Sticks/Solution.java
@@ -0,0 +1,9 @@
+package Amazon._1167_Minimum_Cost_to_Connect_Sticks;
+/*
+
+*/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
