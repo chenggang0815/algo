b49584d06a7860e448f94114935a64d5d01c2c79
chenggang0815 <954036868@qq.com>
2021-09-25 09:54:01 -0400

    _1249_Minimum_Remove_to_Make_Valid_Parentheses

--- Facebook/_1249_Minimum_Remove_to_Make_Valid_Parentheses/Solution.java
+++ Facebook/_1249_Minimum_Remove_to_Make_Valid_Parentheses/Solution.java
@@ -1,7 +1,97 @@
 package Facebook._1249_Minimum_Remove_to_Make_Valid_Parentheses;
 
-public class Solution {
-    public static void main(String[] args) {
+import java.util.HashSet;
+import java.util.Stack;
 
+/*
+1249. Minimum Remove to Make Valid Parentheses
+
+Given a string s of '(' , ')' and lowercase English characters.
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+Formally, a parentheses string is valid if and only if:
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+
+Example 1:
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+Example 2:
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+
+Example 3:
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+
+Example 4:
+Input: s = "(a(b(c)d)"
+Output: "a(b(c)d)"
+*/
+
+/*
+solution:
+ex1: )() => ()
+ex2: (())) => (())
+when number of ) > number of ( => delete )
+
+ex3: ((() => ()
+sometime number of ( > number of ) => use a stack,
+when meet a ( push it to the stack,
+when meet a ) and the number of ) <= (, pop the top ( of stack, the remained ( is need to be deleted
+
+
+stack = [0,1,2]
+when meet )
+stack = [0,1]
+so we need to delete the index of 0 and 1
+
+time complexity is O(n)
+ */
+public class Solution {
+
+    static String minRemoveToMakeValid(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int leftParentheses = 0;
+        int rightParentheses = 0;
+
+        HashSet<Integer> removeIndex = new HashSet<>();
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '('){
+                stack.push(i);
+                leftParentheses++;
+            }else if (s.charAt(i) == ')'){
+                rightParentheses++;
+                if (rightParentheses <= leftParentheses){
+                    stack.pop();
+                    leftParentheses--;
+                    rightParentheses--;
+                }else {
+                    removeIndex.add(i);
+                    rightParentheses--;
+                }
+            }
+        }
+
+        while (!stack.isEmpty()){
+            removeIndex.add(stack.pop());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++){
+            if (!removeIndex.contains(i)){
+                sb.append(s.charAt(i));
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minRemoveToMakeValid("(()))"));
     }
 }
