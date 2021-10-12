5bb514c9be942a0e2213553acac110b48c8ec7b4
chenggang0815 <954036868@qq.com>
2021-10-12 00:46:14 -0400

    _0415_Add_Strings

---
+++ LeetCode/_0043_Multiply_Strings/Solution.java
@@ -0,0 +1,9 @@
+package LeetCode._0043_Multiply_Strings;
+
+public class Solution {
+    public static void main(String[] args) {
+        String num1 = "6";
+        String num2 = "3";
+        System.out.println(num1.charAt(0) - '0');
+    }
+}
