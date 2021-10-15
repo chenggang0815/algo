a95c15aecf8048d4ce39c08a0f572d08b55424e1
chenggang0815 <954036868@qq.com>
2021-10-15 00:12:08 -0400

    _0696_Count_Binary_Substrings

---
+++ Amazon/_0472_Concatenated_Words/Solution.java
@@ -0,0 +1,4 @@
+package Amazon._0472_Concatenated_Words;
+
+public class Solution {
+}
---
+++ Amazon/_1135_Connecting_Cities_With_Minimum_Cost/Solution.java
@@ -0,0 +1,9 @@
+package Amazon._1135_Connecting_Cities_With_Minimum_Cost;
+/*
+
+*/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
