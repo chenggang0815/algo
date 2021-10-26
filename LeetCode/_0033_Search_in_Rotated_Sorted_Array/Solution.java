6325facc0654cd868273aa942ffc8fb13964b56c
chenggang0815 <954036868@qq.com>
2021-10-26 00:16:40 -0400

    _0081_Search_in_Rotated_Sorted_Array_II

--- LeetCode/_0033_Search_in_Rotated_Sorted_Array/Solution.java
+++ LeetCode/_0033_Search_in_Rotated_Sorted_Array/Solution.java
@@ -24,6 +24,15 @@ Output: -1
  */
 
 /*
+Solution:
+Approach 1: binary search time:O(log(n)) space:O(1)
+1. intuition: the array can be split two sorted array
+2. so, the first step, use binary search, we can find the smallest number in the array,
+3. [0, ..,minIndex, ... length-1] => then we get two sorted array nums[0:minIndex-1] and nums[minIndex: length-1]
+4. if   nums[minIndex] <= target <= nums[length - 1] => return binarySearch(nums, minIndex, length - 1, target)
+5. else  return binarySearch(nums, 0, minIndex - 1, target)
+
+Approach 2: binary search one-pass time:O(log(n)) space:O(1)
 思路一：
 1. 将数组从中间分开成左右两部分的时候，一定有一部分的数组是有序的。
 拿[4,5,6,7,0,1,2]来看，从6这个位置分开以后数组变成了[4,5,6]和[7,0,1,2]两个部分，其中左边[4,5,6]这个部分的数组是有序的，其他也是如此。
@@ -47,6 +56,11 @@ public class Solution {
         while (left <= right){
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) return mid;
+            // |_________|___________|
+            // left     pivot      right
+            // mid
+            // if nums[left] == nums[mid], mid - right is not sorted array, so to make sure our search part is always sorted,
+            // we must consider the condition left == mid
             if (nums[left] <= nums[mid]){ //注意细节 =>  nums[0] <= nums[mid] => [3,1] 1
                 if (nums[left] <= target && target < nums[mid]){ //注意细节 =>  nums[left] <= target => [1,3,5] 1
                     right = mid - 1;
@@ -65,7 +79,60 @@ public class Solution {
         return -1;
     }
 
+    //
+    static int search2(int[] nums, int target){
+        int minIndex = 0;
+        int minValue = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++){
+            if (minValue > nums[i]){
+                minIndex = i;
+                minValue = nums[i];
+            }
+        }
+        // 456 0 1234
+        if (target > nums[0]){
+            return binarySearch(nums, 0, minIndex - 1, target);
+        }else{
+            return binarySearch(nums, minIndex, nums.length - 1, target);
+        }
+    }
+
+    static int searchMinIndex(int[] nums){
+        // 3 4 5 6 0 1 2
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid - 1]){
+                return mid;
+            }else{
+                if (nums[mid] < nums[left]){
+                    right = mid - 1;
+                }else {
+                    left = mid + 1;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    static int binarySearch(int[] nums, int left, int right, int target){
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target){ // l mid target r
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println(search(new int[]{3,1}, 3));
+        //System.out.println(search2(new int[]{3,4,5,1,2}, 4));
+        System.out.println(searchMinIndex(new int[]{3,4,5,1,2}));
     }
 }
