f995eead4a95b83342c43f9d3d22b49ca80ff013
chenggang0815 <954036868@qq.com>
2021-10-26 09:32:36 -0400

    _0080_Remove_Duplicates_from_Sorted_Array_II

--- LeetCode/_0080_Remove_Duplicates_from_Sorted_Array_II/Solution.java
+++ LeetCode/_0080_Remove_Duplicates_from_Sorted_Array_II/Solution.java
@@ -10,23 +10,22 @@ It does not matter what you leave beyond the returned k (hence they are undersco
 */
 
 /*
-Solution:
+Solution: time:O(n) space:O(1)
 if k=2, for example
-[1,1,1,1,1,1,2,2,2,2,2,2,3]
-1. when index=0 and index=1 we can remain the element
-2. iterate the array from index=3
-3.
-4. if current number nums[i] == nums[i - k] => i++
+[1, 1,   1,   1, 1, 1,    2,    2, 2, 2, 2, 2, 3]
+     index=2           index=6
+1. make i denote the index for result array, reach time, we compare the current number with nums[i-2]
+2.
+when i=2, current number 1(index=2) == nums[i-2], skip the current number
+when i=2, current number 1(index=3) == nums[i-2], skip the current number
+when i=2, current number 1(index=4) == nums[i-2], skip the current number
+when i=2, current number 1(index=5) == nums[i-2], skip the current number
+when i=2, current number 2(index=6) != nums[i-2] => current number can be add to position i=2 => i++
 
-首先我们先让前 2 位直接保留，得到 1,1
-对后面的每一位进行继续遍历，能够保留的前提是与当前位置的前面 k 个元素不同（答案中的第一个 1），因此我们会跳过剩余的 1，将第一个 2 追加，得到 1,1,2
-继续这个过程，这时候是和答案中的第 2 个 1 进行对比，因此可以得到 1,1,2,2
-这时候和答案中的第 1 个 2 比较，只有与其不同的元素能追加到答案，因此剩余的 2 被跳过，3 被追加到答案：1,1,2,2,3
-
-作者：AC_OIer
-链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/solution/gong-shui-san-xie-guan-yu-shan-chu-you-x-glnq/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+3. if current number == nums[i - k] => skip current number
+   if current number != nums[i - k] => current number can be add to position i,
+                                    => nums[i] = currentNum
+                                    => i++
  */
 public class Solution {
     public int removeDuplicates(int[] nums) {
