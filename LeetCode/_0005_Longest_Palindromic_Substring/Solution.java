f4b102b7ae3671887f4ae0c154ceabd802ddd158
chenggang0815 <954036868@qq.com>
2021-11-11 01:27:44 -0500

    _0005_Longest_Palindromic_Substring

--- LeetCode/_0005_Longest_Palindromic_Substring/Solution.java
+++ LeetCode/_0005_Longest_Palindromic_Substring/Solution.java
@@ -1,16 +1,30 @@
 package LeetCode._0005_Longest_Palindromic_Substring;
 /*
-leetcode 5 - 最长回文子串
-给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+5. Longest Palindromic Substring
+Given a string s, return the longest palindromic substring in s.
 
-示例 1：
-输入: "babad"
-输出: "bab"
-注意: "aba" 也是一个有效答案。
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
 
-示例 2：
-输入: "cbbd"
-输出: "bb"
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+
+Example 3:
+Input: s = "a"
+Output: "a"
+
+Example 4:
+Input: s = "ac"
+Output: "a"
+
+Solution:
+Approach 1: time:o(n^2) space:o(1)
+1. getLen() => make the i-th character is the center of the the palindromic string, return the length of the palindromic string
+2. i i
+   i i+1
 
 思路1： 暴力循环遍历 time: o(n^3) space: o(1)
 思路2： 中心扩散法 time:o(n^2) space:o(1)
@@ -18,6 +32,49 @@ leetcode 5 - 最长回文子串
 
  */
 public class Solution {
+
+    public String longestPalindrome(String s) {
+        int left = 0;
+        int right = 0;
+        //如果是是从i开始扩散，能保证回文串长度是奇数,可以直接 left = i - len/2  right = i + len/2
+        //但是，如果从i和i+1开始扩展，回文串长度为偶数，此时中点在i和i+1之间，但是i的位置在中点的左边，所以计算左边的边界时，需要len-1再除以2
+        // left = i - (len-1)/2 右边不受影响，还是 right = i + len/2
+        //   b  a | a  b len=4 len/2=2
+        //      1        (len-1)/2=1
+        //   b a c a b len=5 len/2=2
+        //       2
+        // len=4  i=1 i+1=2
+        // len=2
+        // left=0
+        // right=3
+
+        for(int i = 0; i < s.length(); i++){
+            int len1 = getLen(s, i, i);
+            int len2 = getLen(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if(len > right - left + 1){
+                left = i - (len - 1) / 2;
+                right = i + len / 2;
+                // left = i - len/2;
+                //  right = i + len/2;
+            }
+        }
+
+        return s.substring(left, right + 1);
+
+    }
+
+    // e a b a d
+    // l   i   r
+    // len = r - l + 1 - 2
+    public int getLen(String s, int left, int right){
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
     //time: o(n^2) space:o(1)
     static String longestPalindrome1(String s) {
         if (s == null || s.length() < 1) return "";
