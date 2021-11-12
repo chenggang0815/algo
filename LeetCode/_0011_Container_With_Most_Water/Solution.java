adaef5223269860166bc7e8e2888025561aabcc9
chenggang0815 <954036868@qq.com>
2021-11-12 11:52:58 -0500

    _0011_Container_With_Most_Water

--- LeetCode/_0011_Container_With_Most_Water/Solution.java
+++ LeetCode/_0011_Container_With_Most_Water/Solution.java
@@ -13,20 +13,18 @@ In this case, the max area of water (blue section) the container can contain is 
  */
 
 /*
-思路1：暴力计算所有的组合 o(n^2)
-思路2：双指针（每次移动短板减少搜索空间） o(n)
+Solution:
+Approach 1：暴力计算所有的组合 o(n^2)
+
+Approach 2：two pointers time: O(n)
 面积取决于短板：
 1. 因此即使长板往内移动时遇到更长的板，矩形的面积也不会改变；遇到更短的板时，面积会变小。
 2. 因此想要面积变大，只能让短板往内移动(因为移动方向固定了)，当然也有可能让面积变得更小，但只有这样才存在让面积变大的可能性
 
 初始状态：
 我们设置两个指针 left 和 right，分别指向数组的最左端和最右端。
-此时，两条垂直线的距离是最远的，若要下一个矩阵面积比当前面积来得大，必须要把 height[left] 和 height[right] 中较短的垂直线往中间移动，看看是否可以找到更长的垂直线。
-
-作者：jalan
-链接：https://leetcode-cn.com/problems/container-with-most-water/solution/shuang-zhi-zhen-jie-fa-by-jalan/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+1. left=0 right=nums.length-1 => 此时，两条垂直线的距离是最远的
+2. 若要下一个矩阵面积比当前面积来得大，必须要把height[left], height[right]中较短的线往中间移动，看看是否可以找到更长的垂直线
  */
 public class Solution {
     static int maxArea(int[] height){
