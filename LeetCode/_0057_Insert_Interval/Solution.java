d21f18daa5beb28e0446c0cc5668e67322bf403d
chenggang0815 <954036868@qq.com>
2021-11-19 21:29:00 -0500

    _0057_Insert_Interval

--- LeetCode/_0057_Insert_Interval/Solution.java
+++ LeetCode/_0057_Insert_Interval/Solution.java
@@ -1,10 +1,8 @@
 package LeetCode._0057_Insert_Interval;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-
 /*
 57. Insert Interval
 
@@ -35,88 +33,80 @@ Output: [[1,7]]
 */
 /*
 Solution:
-1. compare new[start, end] and interval[start, end]
-   if interval[end] < new[start] => res.add(interval)
-   for example: intervals=[[1,2], [3,4], [7,8]] new=[100,101] => [[1,2], [3,4], [7,8], [100,101]]
-                                   => |_______________|          |_______________|
-                                                interval[end] new[start]
-2. until interval[start] < new[end] => interval[end] >= new[start] && interval[start] < new[end]
-                        only interval[start] < new[end]
-                                    => |______________|           |____________|
-                           interval[start]     interval[end]    new[start]    new[end]
+Approach 1: time:O(n) space:O(n)
+iterate the intervals, for each interval and newInterval, we have 3 case:
+case 1 => if interval[1] < newInterval[1]
+       => res.add(interval)
+                                        |_________|
+                               interval[0]     interval[1]
+                                                                     |________|
+                                                          newInterval[0]   newInterval[1]
 
+case 2 => else if newInterval[1] < interval[0]
+       => res.add(newInterval)
+       => newInterval = interval //update newInterval
+                                                                    |_________|
+                                                          interval[0]     interval[1]
+                                     |__________|
+                          newInterval[0]   newInterval[1]
 
-                                    => |______________|_____________|____________|
-                           interval[start]     new[start]    interval[end]    new[end]
-                    Math.min(newInterval[0], intervals[i][0])
-                    Math.max(newInterval[1], intervals[i][1])}
-   for example: intervals=[1,2, [3,4], [100, 105]] new=[100, 101]
+case 3 => else, merge interval and newInterval
+        => newInterval[0] = Math.min(newInterval[0], intervals[i][0])
+        => newInterval[1] = Math.max(newInterval[1], intervals[i][1])}
+                                        |__________________|
+                                     interval[0]         interval[1]
+                                                     |__________________|
+                                              newInterval[0]        newInterval[1]
+
+                                                      |__________________|
+                                                   interval[0]         interval[1]
+                                       |__________________|
+                              newInterval[0]        newInterval[1]
+
+Approach 2:
+1. compare interval[1] and newInterval[0]
+   while(interval[1] < newInterval[0]) => res.add(interval)
+
+2. until interval[0] < newInterval[1]
+  2.1 because the condition interval[end] >= new[start] already exists
+  2.2 then we have condition, interval[end] >= new[start] && interval[start] < new[end]
+  2.3 these two intervals must have overlap
 */
 public class Solution {
+    // Approach1
     static int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> res = new ArrayList<>();
-        //
-        int flag = 0;
         for(int[] interval: intervals){
-            if(flag == 1){
-                res.add(interval);
-                continue;
-            }
-            if(interval[1] < newInterval[0]){
-                res.add(interval);
-                continue;
-            }
-            System.out.print(Arrays.toString(newInterval));
-            System.out.println("=====");
-            if(interval[0] > newInterval[1]){
+            if(interval[1] < newInterval[0]) res.add(interval);
+            else if (newInterval[1] < interval[0]){
                 res.add(newInterval);
-                res.add(interval);
-                flag = 1;
-                continue;
-            }
-            if(interval[0] > newInterval[0] && interval[1] < newInterval[1]) continue;
-            if(interval[1] >= newInterval[0]){
-                newInterval[0] = interval[0];
-                continue;
-            }
-            if(interval[0] <= newInterval[1]){
-                newInterval[1] = interval[1];
+                newInterval = interval;
+            }else{
+                newInterval[0] = Math.min(interval[0], newInterval[0]);
+                newInterval[1] = Math.max(interval[1], newInterval[1]);
             }
         }
-
+        res.add(newInterval);
         int[][] nums = new int[res.size()][2];
-        for(int i = 0; i < res.size(); i++){
-            nums[i] = res.get(i);
-        }
+        for(int i = 0; i < res.size(); i++) nums[i] = res.get(i);
 
         return nums;
     }
 
+    // Approach2
     static int[][] insert2(int[][] intervals, int[] newInterval) {
         List<int[]> result = new LinkedList<>();
         int i = 0;
-
-        while (i < intervals.length && intervals[i][1] < newInterval[0]){
-            result.add(intervals[i++]);
-        }
-
-        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-            newInterval = new int[]{
-                    Math.min(newInterval[0], intervals[i][0]),
-                    Math.max(newInterval[1], intervals[i][1])};
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) result.add(intervals[i++]);
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
         }
-
-        System.out.println(Arrays.toString(newInterval));
-
         result.add(newInterval); // add the union of intervals we got
-
         while (i < intervals.length) result.add(intervals[i++]);
-
         int[][] nums = new int[result.size()][2];
-        for(int j = 0; j < result.size(); j++){
-            nums[j] = result.get(j);
-        }
+        for(int j = 0; j < result.size(); j++) nums[j] = result.get(j);
 
         return nums;
     }
