31a1132ca3950b532ca4d353c70593711f6537fd
chenggang0815 <954036868@qq.com>
2021-11-21 20:39:29 -0500

    _0206_Reverse_Linked_List

--- LeetCode/_0206_Reverse_Linked_List/Solution.java
+++ LeetCode/_0206_Reverse_Linked_List/Solution.java
@@ -1,13 +1,42 @@
 package LeetCode._0206_Reverse_Linked_List;
 /*
-206 Reverse Linked List 反转单链表
-Reverse a singly linked list.
-Example:
-Input: 1->2->3->4->5->NULL
-Output: 5->4->3->2->1->NULL
-思路:
-1. 三指针法
-2. 递归
+206. Reverse Linked List
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Solution
+Approach 1 Iterative
+null   1 -> 2 -> 3 -> 4 -> 5
+pre   cur  next
+1. pre = null cur = head
+2. reverse cur and pre
+    cur.next = pre
+    pre = cur
+    cur = cur.next => false, because cur.next already point to pre, it exists cycle
+3. so we need to record next node first.
+    ListNode next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+4. return pre
+
+Approach 2 Recursion
+1 -> 2 -> 3 -> 4 -> 5
+newHead = reverse(node) => if(node == null || node.next == null) return the tail node(5) of original list
+node = 5, we can't return to 4
+
+newHead = reverse(node.next) => if(node == null || node.next == null) return the tail node(5) of original list
+node = 4 => node.next.next = next => 5.next=4
+1 -> 2 -> 3 -> 4 <- 5
+
+node.next.next = node
+node.next = null
+ => for the original head node
+    node=1 node.node.next = node => 2->1 1->2
+    so we need node.next = null => null <- 1 <- 2
 
 92. Reverse Linked List II
 */
@@ -41,24 +70,8 @@ public class Solution {
         }
     }
 
-/*
-三指针
-  null  1 -> 2 -> 3 -> 4
-  pre  cur  next
-  pre = null
-  cur = head
-  next = cur.next
-  cur.next = pre
-  pre = cur
-  cur = next
-  null <- 1    2 -> 3 -> 4
-          pre cur  next
-  null <- 1 <- 2  3 -> 4
-              pre cur next
 
-*/
-
-    public static ListNode reverseList2(ListNode head){
+    public static ListNode reverseList1(ListNode head){
         ListNode cur = head;
         ListNode pre = null;
         while (cur != null){
@@ -68,32 +81,18 @@ public class Solution {
             cur = next;
         }
 
-        return  pre;
+        return pre;
     }
 
-    //递归法
-    private static ListNode reverseList1(ListNode head){
-        if (head == null || head.next == null){
-            return head;
-        }
+    //recursion
+    private static ListNode reverseList2(ListNode head){
+        if (head == null || head.next == null) return head;
 
-        /*
-        1->2->3->4->5
-        head = 4 => r(head.next) => return 5
-        4.next.next = 4  => 5.next = 4
-        4.next = null
-        null<-4<-5
-        3.next.next = 3  => 4.next = 3    4
-        3.next = null
-        null<-3<-4<-5
-        ...
-        null<-1<-2<-3<-4<-5
-         */
-        ListNode newhead = reverseList1(head.next);
+        ListNode newHead = reverseList1(head.next);
         head.next.next = head;
         head.next = null;
 
-        return newhead;
+        return newHead;
     }
 
 
