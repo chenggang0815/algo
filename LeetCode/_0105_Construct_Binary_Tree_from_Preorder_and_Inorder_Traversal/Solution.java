8e361068a9b4b60e9403b77d9727f2d85d972686
chenggang0815 <954036868@qq.com>
2021-11-25 11:47:58 -0500

    _0105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal

--- LeetCode/_0105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Solution.java
+++ LeetCode/_0105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Solution.java
@@ -6,8 +6,12 @@ Given preorder and inorder traversal of a tree, construct the binary tree.
 
 Note:
 You may assume that duplicates do not exist in the tree.
- */
-import java.util.Arrays;
+*/
+/*
+Solution:
+time:O(n) space:O(n)
+similar to question 105
+*/
 import java.util.HashMap;
 
 public class Solution {
@@ -19,73 +23,35 @@ public class Solution {
             this.val = val;
         }
     }
-
-    static int rootIndex(int[] preorder, int[] inorder){
-        int root = preorder[0];
-        for (int i = 0; i < inorder.length; i++){
-            if (root == inorder[i]) return i;
-        }
-
-        return -1;
-    }
-
-    static TreeNode buildTree1(int[] preorder, int[] inorder) {
-        if(preorder.length == 0) return null;
-
-        TreeNode root = new TreeNode(preorder[0]);
-
-        int rootIndex = rootIndex(preorder, inorder);
-
-        root.left = buildTree1(Arrays.copyOfRange(preorder,1,rootIndex + 1),Arrays.copyOfRange(inorder,0,rootIndex));
-        root.right = buildTree1(Arrays.copyOfRange(preorder,rootIndex + 1,preorder.length),Arrays.copyOfRange(inorder,rootIndex + 1,inorder.length));
-
-        return root;
-    }
-
-    static TreeNode buildTree(int[] preOrder, int[] inorder){
+    //  preorder => root-left-right
+    //  inorder =>  left-root-right
+    //  postorder => left-right-root
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
         HashMap<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < inorder.length; i++){
             map.put(inorder[i], i);
         }
 
-        return buildTree2(preOrder, inorder, map);
+        return build(inorder, 0, inorder.length - 1, preorder, 0, preorder.length - 1, map);
     }
 
-    static TreeNode buildTree2(int[] preorder, int[] inorder, HashMap<Integer, Integer> map) {
-        if(preorder.length == 0){
-            System.out.println("hhh");
-            return null;
-        }
-        TreeNode root = new TreeNode(preorder[0]);
+    public TreeNode build(int[] inorder, int inorderStart, int inorderEnd, int[] preorder, int preStart, int preEnd, HashMap<Integer, Integer> map){
+        if (inorderStart > inorderEnd) return null;
 
-        int rootIndex = map.get(preorder[0]);
-        if (preorder[0] == 12) System.out.println(rootIndex);
-
-        root.left = buildTree2(Arrays.copyOfRange(preorder,1,rootIndex + 1),Arrays.copyOfRange(inorder,0,rootIndex), map);
-        root.right = buildTree2(Arrays.copyOfRange(preorder,rootIndex + 1,preorder.length),Arrays.copyOfRange(inorder,rootIndex + 1,inorder.length), map);
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int rootIndex = map.get(root.val);
+        // pre_left_start = preStart + 1
+        // pre_left_end =  pre_left_start + (rootIndex - inorderStart + 1 - 2)
+        //              = preStart + 1 + (rootIndex - inorderStart - 1) = preStart + rootIndex - inorderStart
+        // pre_right_start = pre_left_end + 1 = preStart + rootIndex - inorderStart + 1
+        // pre_right_end = preEnd
+                                                                                    //pre left start = preStart + 1
+        root.left = build(inorder, inorderStart, rootIndex - 1, preorder, preStart + 1, preStart + rootIndex - inorderStart, map);
+        root.right = build(inorder, rootIndex + 1, inorderEnd, preorder, preStart + rootIndex - inorderStart + 1, preEnd, map);
 
         return root;
     }
 
-
-
-    /*
-    遍历验证
-     */
-    static void preOrder(TreeNode root){
-        if (root == null) return;
-        System.out.println(root.val);
-        preOrder(root.left);
-        preOrder(root.right);
-    }
-
-    static void inOrder(TreeNode root){
-        if (root == null) return;
-        preOrder(root.left);
-        System.out.println(root.val);
-        preOrder(root.right);
-    }
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3);
@@ -106,10 +72,6 @@ public class Solution {
         int[] pre = new int[]{3,9,12,13,20,15,7};
         int[] in = new int[]{12,9,13,3,15,20,7};
 
-        preOrder(root);
-        System.out.println("=====");
-        TreeNode node = buildTree(pre, in);
-        preOrder(node);
 
 
     }
