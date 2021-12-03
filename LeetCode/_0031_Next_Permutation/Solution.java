4fec50fe510208310482ca4ea4795e113d5e1760
chenggang0815 <954036868@qq.com>
2021-12-03 12:54:24 -0500

    _0031_Next_Permutation

--- LeetCode/_0031_Next_Permutation/Solution.java
+++ LeetCode/_0031_Next_Permutation/Solution.java
@@ -1,68 +1,79 @@
 package LeetCode._0031_Next_Permutation;
 /*
-31. 下一个排列
+31. Next Permutation
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
+The replacement must be in place and use only constant extra memory.
 
-实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
-如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
-必须原地修改，只允许使用额外常数空间。
+Example 1:
+Input: nums = [1,2,3]
+Output: [1,3,2]
 
-以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
-1,2,3 → 1,3,2
-3,2,1 → 1,2,3
-1,1,5 → 1,5,1
-
- */
-
-import java.util.Arrays;
+Example 2:
+Input: nums = [3,2,1]
+Output: [1,2,3]
+*/
 
 /*
-思路
-1 暴力法 time：o(n!) space: o(n)
-
-2. 一次遍历
-1, 2, 3, 8, 5, 7, 6, 4
-从后向前查找第一个相邻升序的元素对 (i, i+1)，满足 A[i] < A[i+1]。此时 [i+1,end) 必然是降序
-在 [i+1,end) 从后向前查找第一个满足 A[i] < A[j] 的 j。A[i]、A[j] 分别就是上文所说的「小数」、「大数」
-将 A[i] 与 A[j] 交换
-可以断定这时 [i+1,end) 必然是降序，逆置 [i+1,end)，使其升序
-如果在步骤 1 找不到符合的相邻元素对，说明当前 [begin,end) 为一个降序顺序，则直接跳到步骤 4
+Solution:
+Approach 1
+1. iterate the array from right to left
+2. [1,2,3,4,5] for example, we iterate from right, we can know if we swap 5 and 4, we can get 1 2 3 5 4 which is the answer
+3. but the situation is more complex, because we will have these situations
+    3.1 [5,4,3,2,1] => the array is sorted in descending order, we just need to reverse the array, the answer is [1,2,3,4,5]
+    3.2 [1,2,3,4,5] => we find the first pair to meet nums[i] > nums[j], and then switch nums[i] and nums[j], but there will have a issue
+    3.3 [1,2,3,6,5,4] => the first pair 3 < 6, if we swap 3 and 6, we have
+        => [1,2,  3,  6, 5,  4] => it's not meet the next permutation, the next permutation is [1,2,3,4,5,6]
+                i-1  i     length-1
+        => yes, we should sorted from i to nums.length-1 in ascending order
+        => because from i to length-1 is descending order, so we just reverse from i to length-1
+    3.4 and j is not always equal i, for example => [1,  2,  3,  5,  4,  3,  2]
+                                                            i-1  i   j
+        => first step, we find 3 < 5, the first pair
+        => second step, we find nums[j]=4 > nums[i-1]=3
+        => we swap nums[i-1] and nums[j]
  */
 public class Solution {
-    static void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        while (i >= 0 && nums[i + 1] <= nums[i]){
+    // 6 4 3 2 1
+    // 1 2 3 4 6 => 1 2 3 6 4
+    //        4<6
+    // 1 2 3 6 4 => 1 2 4 6 3 => 1 2 4 3 6
+    // 1 2 3 6 5 3 1 => 1 2 5 6 3 3 1 => 1 2 5 1 3 3 6
+    //      <  >
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 1;
+        while(i > 0 && nums[i - 1] >= nums[i]){
             i--;
         }
 
-        if (i >= 0){
-            int j = nums.length - 1;
-            while (j >= 0 && nums[j] <= nums[i]){
-                j--;
-            }
-            swap(nums, i, j);
+        if(i == 0){
+            reverse(nums, 0, nums.length - 1);
+            return;
         }
 
-        reverse(nums, i + 1);
+        int j = nums.length - 1;
+        while(j > i - 1 && nums[j] <= nums[i - 1]){
+            j--;
+        }
+        swap(nums, i - 1, j);
+        reverse(nums, i, nums.length - 1);
     }
 
-    static void swap(int[] nums, int i, int j){
+    public void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    static void reverse(int[] nums, int i){
-        int j = nums.length - 1;
-        while (i < j){
-           swap(nums, i, j);
-           i++;
-           j--;
+    public void reverse(int[] nums, int left, int right){
+        while(left < right){
+            swap(nums, left, right);
+            left++;
+            right--;
         }
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3, 8, 5, 7, 6, 4};
-        nextPermutation(nums);
-        System.out.println(Arrays.toString(nums));
+
     }
 }
