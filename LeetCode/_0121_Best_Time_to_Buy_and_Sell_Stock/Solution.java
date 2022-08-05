16b224b659b4e705fd8d4a4c9c8cbd8e4addea83
chenggang0815 <954036868@qq.com>
2022-08-04 22:13:27 -0700

    2022-08-05

--- LeetCode/_0121_Best_Time_to_Buy_and_Sell_Stock/Solution.java
+++ LeetCode/_0121_Best_Time_to_Buy_and_Sell_Stock/Solution.java
@@ -17,7 +17,7 @@ Example 2:
 Input: [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
- */
+*/
 /*
 Solution:
 1. maintain a minimum buy price => buyPrice
