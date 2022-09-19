c9ba5566f5989169a7b4114162a2b3fb1491bfdf
chenggang0815 <954036868@qq.com>
2022-09-18 23:24:22 -0400

    _0611_Valid_Triangle_Number

---
+++ LeetCode/_0611_Valid_Triangle_Number/Solution.java
@@ -0,0 +1,59 @@
+package LeetCode._0611_Valid_Triangle_Number;
+
+import java.util.Arrays;
+
+/*
+611. Valid Triangle Number
+
+Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+
+Example 1:
+Input: nums = [2,2,3,4]
+Output: 3
+Explanation: Valid combinations are:
+2,3,4 (using the first 2)
+2,3,4 (using the second 2)
+2,2,3
+
+Example 2:
+Input: nums = [4,2,3,4]
+Output: 4
+
+Constraints:
+1 <= nums.length <= 1000
+0 <= nums[i] <= 1000
+*/
+/*
+Approach: Sort + two pointer
+similar question: 3 sum
+time: O(n^2)
+space: O(log(n)). Sorting takes O(log(n)) space
+ */
+public class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        // 2 3 4 4 => 2 3 4 / 2 3 4 / 3 4 4 / 2 4 4
+        // l   r i => 2 4 4 / 3 4 4
+        // l r   i => 2 3 4
+        // l r i   => 2 3 4
+        int res = 0;
+        for(int i = nums.length - 1; i >= 2; i--){
+            int left = 0;
+            int right = i - 1;
+            while(left < right){
+                if(nums[left] + nums[right] > nums[i]){
+                    res += right - left;
+                    right--;
+                }else{
+                    left++;
+                }
+            }
+        }
+
+        return res;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
