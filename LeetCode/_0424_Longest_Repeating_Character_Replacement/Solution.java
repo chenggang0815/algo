625c75305038ee3167b6e10668862cdc086398c1
chenggang0815 <954036868@qq.com>
2022-09-21 20:19:30 -0400

    _0424_Longest_Repeating_Character_Replacement

--- LeetCode/_0424_Longest_Repeating_Character_Replacement/Solution.java
+++ LeetCode/_0424_Longest_Repeating_Character_Replacement/Solution.java
@@ -27,22 +27,26 @@ s consists of only uppercase English letters.
 /*
 Solution:
 Approach 1: two pointer - sliding window
+1. maintain a window, make sure this can change no more than k characters to become a valid string
+2. if the length of current window - the number of most frequent character > k, which means it is not a valid window
+3. we need to contract the window and update frequency
+4. else it is a valid window, we can expend the window and update frequency
 https://leetcode.com/problems/longest-repeating-character-replacement/discuss/358879/Java-Solution-Explained-and-Easy-to-Understand-for-Interviews
 */
 public class Solution {
     int characterReplacement(String s, int k){
-        int[] charCnt = new int[26];
-        int mostFreqLetter = 0;
-        int res = 0;
+        int[] cnt = new int[26];
         int left = 0;
-        for (int i = 0; i < s.length(); i++){
-            charCnt[s.charAt(i)]++;
-            mostFreqLetter = Math.max(mostFreqLetter, charCnt[s.charAt(i)]);
-            if (i - left + 1 - mostFreqLetter > k){
-                charCnt[s.charAt(left)]--;
+        int maxFreq = 1;
+        int res = 1;
+        for(int i = 0; i < s.length(); i++){
+            cnt[s.charAt(i) - 'A']++;
+            maxFreq = Math.max(maxFreq, cnt[s.charAt(i) - 'A']);
+            // length = 5 maxFreq = 3 2 >k
+            if(i - left + 1 - maxFreq > k){
+                cnt[s.charAt(left) - 'A']--;
                 left++;
             }
-
             res = Math.max(res, i - left + 1);
         }
 
