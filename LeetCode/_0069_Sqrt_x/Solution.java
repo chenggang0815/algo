d088c2662f2ff30d501292a58c7877dfccade0eb
chenggang0815 <954036868@qq.com>
2022-10-01 19:43:26 -0400

    _0069_Sqrt_x

--- LeetCode/_0069_Sqrt_x/Solution.java
+++ LeetCode/_0069_Sqrt_x/Solution.java
@@ -22,24 +22,29 @@ public class Solution {
     目标：找出最大的mid，使得mid < x/mid
      */
     //time: o(log2(x)) time: o(1)
-    static public int mySqrt(int x) {
+    // x = 64
+    // mid * mid < 64 => left = mid + 1
+    // mid * mid >= 64 => right = mid - 1, res = mid
+    // we want to find the first mid, make mid * mid >= x
+    public int mySqrt(int x) {
         int left = 1;
         int right = x;
+        int res = 1;
         while(left <= right){
-            int mid = left + (right-left)/2;
-            if(mid == x/mid) return mid;
-            else if(mid < x/mid){
+            int mid = left + (right - left) / 2;
+            if(mid == x / mid) return mid;
+            if(mid  > x / mid){
+                right = mid - 1;
+                res = mid;
+            }else{
                 left = mid + 1;
-            }
-            else if(mid > x/mid){
-                right = mid -1;
             }
         }
 
-        return left-1;
+        return res - 1;
     }
 
     public static void main(String[] args) {
-        System.out.println(mySqrt(9));
+        //System.out.println(mySqrt(9));
     }
 }
